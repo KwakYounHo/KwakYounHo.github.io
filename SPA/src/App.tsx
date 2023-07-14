28c657bca64032e3a3ac3d6c10ccceac8d57b123
KwakYounHo <kwakyh951@gmail.com>
2023-07-14 11:56:21 +0900

    테스트코드 삽입

--- SPA/src/App.tsx
+++ SPA/src/App.tsx
@@ -6,10 +6,11 @@ interface Props {}
 const App:React.FC<Props> = () => {
   return (
     <>
-      <h1>아니 이게 벌써</h1>
+      <h1>콘솔로그.헬로월드</h1>
+      {/* <h1>아니 이게 벌써</h1>
       <Routes>
         <Route path='/' element={<Main />} />
-      </Routes>
+      </Routes> */}
     </>
   )
 }
