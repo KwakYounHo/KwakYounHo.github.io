7aa249a9262f15c259129103242c7cfb357cfddb
KwakYounHo <kwakyh951@gmail.com>
2023-07-14 11:58:27 +0900

    테스트 빌드

--- SPA/src/App.tsx
+++ SPA/src/App.tsx
@@ -5,13 +5,9 @@ import Main from './component/mainPage/main'
 interface Props {}
 const App:React.FC<Props> = () => {
   return (
-    <>
-      <h1>콘솔로그.헬로월드</h1>
-      {/* <h1>아니 이게 벌써</h1>
-      <Routes>
-        <Route path='/' element={<Main />} />
-      </Routes> */}
-    </>
+    <Routes>
+      <Route path='/' element={<Main />} />
+    </Routes>
   )
 }
 export default App;
