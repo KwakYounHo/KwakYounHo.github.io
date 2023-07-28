eaae191aec5ba07970b85faac515b7bd87f88a98
곽윤호 <kwakdm95@gmail.com>
2023-07-28 15:50:44 +0900

    mk board : base setup insert img to board

--- tools/src/screens/Post/PostBoard.tsx
+++ tools/src/screens/Post/PostBoard.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Content, ContentH2, HeaderContainer, HeaderImage } from '../../styles/Header/LectureHeader.style'
+import header from '../../assets/img/React_Banner.png'
 
 interface PostBoardProps {}
 const PostBoard: React.FC<PostBoardProps> = () => {
   return (
-    <div>PostBoard</div>
+    <HeaderContainer>
+      <HeaderImage src={header} />
+      <Content>
+        <ContentH2>
+
+        </ContentH2>
+      </Content>
+    </HeaderContainer>
   )
 }
 
---
+++ tools/src/styles/Header/LectureHeader.style.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+
+`;
+
+export const Content = styled.div`
+  position: absolute;
+`
+
+export const HeaderImage = styled.img`
+  vertical-align: middle;
+`
+
+export const ContentH2 = styled.h2`
+  font-size: 5rem;
+  font-family: 'gugi';
+`
